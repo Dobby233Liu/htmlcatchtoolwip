14591a126a6b6f88bea288128b2364b8c1090b53
Liu-Wen-Yuan <15816141883@163.com>
2017-09-24 03:04:42 -0500

    Update catch.js

--- catch.js
+++ catch.js
@@ -1,12 +1,12 @@
-function GetText(url){
+function GetText(){
     var request = new XMLHttpRequest();       
-    request.open("GET", url);    
+    request.open("GET", document.getElementById("GENC_Input").value);    
     request.onreadystatechange = function(){  
     //if( request.readyState !== 4 ) return;        
     //if( timeout ) return;        
     //clearTimeout( timer );       
     if(request.status === 200){
-    return request.responseText;        
+    document.getElementById("GENC_Output").value = request.responseText;        
     }   
     }    
     request.send(null);
