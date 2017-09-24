b2977b5dc55dcfb0ef8d6143e1ba90cd7b7d7804
Liu-Wen-Yuan <15816141883@163.com>
2017-09-24 03:45:18 -0500

    Update catch.js

--- catch.js
+++ catch.js
@@ -1,16 +1,13 @@
-function GetText(){
+function main(){
     var request = new XMLHttpRequest();      
-    var GCF_URL = document.getElementById("url").value
+    var GCF_URL = document.getElementById("url").value;
     // credits http://www.cnblogs.com/charling/p/3579704.html
     request.open("GET", GCF_URL); 
-    request.onreadystatechange = function(){  
-    //if( request.readyState !== 4 ) return;        
-    //if( timeout ) return;        
-    //clearTimeout( timer );       
+    request.onreadystatechange = function(){
     // its showtime
         var WhatsGetOut = request.responseText
-    document.getElementById("sc").value = WhatsGetOut;    ///    Got an SC (Source)
+    document.getElementById("sc").value = WhatsGetOut    ///    Got an SC (Source)
     // end
     }  
-    return
+    // return
     }
