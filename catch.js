a02e2d9ec61ec8dd7aca62388121b2e82884636e
Liu-Wen-Yuan <15816141883@163.com>
2017-09-25 16:06:59 +0800

    Add files via upload

--- catch.js
+++ catch.js
@@ -1,13 +1,14 @@
-function main(){
-    var request = new XMLHttpRequest();      
-    var GCF_URL = document.getElementById("url").value
+﻿function mainc(){
+        var request = new XMLHttpRequest();      
+        var URL = String(document.getElementById("url").value);
     // credits http://www.cnblogs.com/charling/p/3579704.html
-    request.open("GET", GCF_URL);
-    var WhatsGetOut = request.responseText
+    request.open("GET", URL);
+        request.send(null);
+                            var WhatsGetOut = String(request.responseText);
     // orig fn start
     // its showtime
-    document.getElementById("sc").innerHTML = String(WhatsGetOut)   
+    document.getElementById("sc").innerHTML = String(WhatsGetOut);
         //    Got an SC (Source)
     // end
     // orig fn end
-    }
+                }
