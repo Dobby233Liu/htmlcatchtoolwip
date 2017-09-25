db6852fecf150516d61d0f33fff40b457bc4582b
Liu-Wen-Yuan <15816141883@163.com>
2017-09-25 00:35:27 -0500

    Update catch.js

--- catch.js
+++ catch.js
@@ -2,7 +2,7 @@ function main(){
     var request = new XMLHttpRequest();      
     var GCF_URL = document.getElementById("url").value
     // credits http://www.cnblogs.com/charling/p/3579704.html
-    request.open("GET", GCF_URL)
+    request.open("GET", GCF_URL);
     var WhatsGetOut = request.responseText
     // orig fn start
     // its showtime
