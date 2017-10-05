c26e5da2e230b3b0ca339b37ace9c470744686fc
Liu-Wen-Yuan <15816141883@163.com>
2017-10-05 04:01:56 -0500

    Update catch.js

--- catch.js
+++ catch.js
@@ -2,7 +2,7 @@
         var request = new XMLHttpRequest();      
         var URL = String(document.getElementById('url').value);
     // credits http://www.cnblogs.com/charling/p/3579704.html
-    request.open("GET", URL);
+    request.open('GET', URL);
         request.send(null);
                             var WhatsGetOut = String(request.responseText);
     // orig fn start
